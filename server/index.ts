7025ff43ad64a13ed462c0d8c2dcac225bb15aa4
John Glen Siy <johnglen_siy@berkeley.edu>
2026-05-19 18:54:28 -0700

    feat(wip): add playwright setup on express spin-up

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,25 @@
-import express from 'express'
+import express from 'express';
+import { chromium, Browser, BrowserContext, Page } from 'playwright';
+import eventsRoutes, { startWatching } from './routes/events.routes.ts';
 
-const app = express()
+const app = express();
+const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
+
+export let browser: Browser;
+export let context: BrowserContext;
+export let page: Page;
+
+browser = await chromium.launch({ channel: 'chrome', headless: false })
+context = await browser.newContext();
+page = await context.newPage();
+await page.goto("https://www.atptour.com/en/scores/stats-centre/live/2026/322/MS029?tab=CourtVision")
+// startWatching();
+
+app.use('/', eventsRoutes);
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
