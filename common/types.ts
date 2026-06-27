9a9c6c0aaf12a3a6a6067e1ac25ffd7d5e511dd8
John Glen Siy <johnglen_siy@berkeley.edu>
2026-06-26 23:53:03 -0400

    new DTO types MatchScheduled ScoreUpdated and MatchFinished for live match streaming

--- common/types.ts
+++ common/types.ts
@@ -57,3 +57,48 @@ export interface TournamentState {
     matches: MatchStateOld[];
 }
 
+
+export interface SideScore {
+  gameScore: string;
+  setScores: (number | null)[];
+}
+
+export interface MatchScheduled {
+  type: 'scheduled';
+  matchId: string;
+  tour: Tour;
+  tournament: string;
+  court: string;
+  scheduledTime: string;
+  round: string;
+  playerA: PlayerStateOld;
+  playerB: PlayerStateOld;
+}
+
+export interface ScoreUpdated {
+  type: 'score';
+  matchId: string;
+  updateId: string;
+
+  // point-specific
+  playerScore: SideScore;
+  opponentScore: SideScore; // resulting score
+  scorer: 'p' | 'o';        // scorer of the point that was just played
+  server: 'p' | 'o';        // server of the point that was just played
+  result: string;
+  rallyLength: number;
+
+  // match-specific
+  isGameComplete: boolean;
+  isSetComplete: boolean;
+  timeElapsedInSeconds: number;
+  matchStatus: string;
+}
+
+export interface MatchFinished {
+  type: 'finished';
+  matchId: string;
+
+}
+
+export type MatchEvent = MatchScheduled | ScoreUpdated | MatchFinished;
