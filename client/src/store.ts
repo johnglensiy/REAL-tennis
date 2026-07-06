ed4826c7d286e879e8ff6c6c65b980d4619ab0e2
John Glen Siy <johnglen_siy@berkeley.edu>
2026-07-06 13:08:56 -0700

    wip: register matchesSlice to store

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,16 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import type { Action } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import type { Action } from "@reduxjs/toolkit";
 
-import tournamentsReducer from './features/tournament-tracker/tournamentsSlice'
+import tournamentsReducer from "./features/tournament-tracker/tournamentsSlice";
+import matchesReducer from "./features/tournament-tracker/matchesSlice";
 
 export const store = configureStore({
-    reducer: {
-        // Declare that `state.match` will be updated by the `counterReducer` function
-        tournaments: tournamentsReducer
-    }
-})
+  reducer: {
+    // Declare that `state.match` will be updated by the `counterReducer` function
+    tournaments: tournamentsReducer,
+    matches: matchesReducer,
+  },
+});
 
-export type AppStore = typeof store
-export type AppDispatch = typeof store.dispatch 
-export type RootState = ReturnType<typeof store.getState>
-
+export type AppStore = typeof store;
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
